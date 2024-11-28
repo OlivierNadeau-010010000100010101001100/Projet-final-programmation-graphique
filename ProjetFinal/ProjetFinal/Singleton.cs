2b266af32636eea5212e49bf63da8e7a47af4fdc
DylannPalardy <dylann.palardy.01@edu.cegeptr.qc.ca>
2024-11-28 15:36:59 -0500

    .

--- ProjetFinal/ProjetFinal/Singleton.cs
+++ ProjetFinal/ProjetFinal/Singleton.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +12,36 @@ namespace ProjetFinal
 {
     internal class Singleton
     {
+
+
         static Singleton instance = null;
+        private TextBlock _messageErreur;
 
-        public string conn {  get; private set; }
+        public string conn { get; private set; }
 
-        private Singleton() 
+        private Singleton()
         {
-            conn = 
+            conn = "Server=cours.cegep3r.info;Database=420345ri_gr00002_2011835-dylann-palardy;Uid=2011835;Pwd=2011835;";
         }
+
+        public static Singleton Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        public void SetMessageErreur(TextBlock messageErreur)
+        {
+            _messageErreur = messageErreur;
+        }
+
+
+        
+
+
+
     }
 }
