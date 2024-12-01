b3894882cf25d1fb12bc038723152b93a656bfc1
DylannPalardy <dylann.palardy.01@edu.cegeptr.qc.ca>
2024-12-01 00:06:08 -0500

    .

--- ProjetFinal/ProjetFinal/PageUser.xaml.cs
+++ ProjetFinal/ProjetFinal/PageUser.xaml.cs
@@ -39,6 +39,7 @@ namespace ProjetFinal
             {
                 xamlAffichageLorsqueConnected();
             } else
+            {
                 xamlAffichageLorsqueDisconnected();
             }
         }
