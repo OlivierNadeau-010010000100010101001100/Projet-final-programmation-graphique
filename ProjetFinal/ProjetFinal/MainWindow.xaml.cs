f45d702c66f60d6d010145f5264d8e1392ea68f4
DylannPalardy <dylann.palardy.01@edu.cegeptr.qc.ca>
2024-12-05 21:29:30 -0500

    .

--- ProjetFinal/ProjetFinal/MainWindow.xaml.cs
+++ ProjetFinal/ProjetFinal/MainWindow.xaml.cs
@@ -69,6 +69,9 @@ namespace ProjetFinal
                 case "iQuitter":
                     Application.Current.Exit();
                     break;
+                case "suppAdherent":
+                    mainFrame.Navigate(typeof(PageSupprimerAdherent));
+                    break;
                 default:
                     break;
             }
