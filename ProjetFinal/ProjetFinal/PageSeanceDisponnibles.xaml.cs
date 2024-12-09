f8337c5056458c919bb74f497d4432f4aceb1461
Olivier <oliviernadeauallo@gmail.com>
2024-12-09 16:36:25 -0500

    eeee

--- ProjetFinal/ProjetFinal/PageSeanceDisponnibles.xaml.cs
+++ ProjetFinal/ProjetFinal/PageSeanceDisponnibles.xaml.cs
@@ -62,6 +62,8 @@ namespace ProjetFinal
         {
             var seance = (Seance)LVseances.SelectedItem;
             Singleton.Instance().AjoutInscription(seance.Id);
+
+            Frame.Navigate(typeof(PageGestionSeanceClient));
         }
 
         private void LVseances_SelectionChanged(object sender, SelectionChangedEventArgs e)
